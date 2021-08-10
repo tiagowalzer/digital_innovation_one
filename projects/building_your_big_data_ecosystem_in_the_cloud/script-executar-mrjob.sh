7725815de8033a560bc412108bf8c54fb24c714c
Tiago Walzer <tiagowalzer@outlook.com>
2021-08-10 11:05:05 -0300

    Delete files

--- projects/building_your_big_data_ecosystem_in_the_cloud/script-executar-mrjob.sh
+++
@@ -1,4 +0,0 @@
-python3 mr-analise-palavras-livro.py \
-    -r emr s3://datalake-exemplo/data/TheAdventuresOfSherlockHolmes-ArthurConanDoyle.txt \
-    --output-dir=s3://datalake-exemplo/outputs/job1/ \
-    --cloud-tmp-dir=s3://datalake-exemplo/temp/
